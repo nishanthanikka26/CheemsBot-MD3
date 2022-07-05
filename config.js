973f850551ec3ffe79cdf6ab9335808b086e4668
nishanthanikka26 <108712827+nishanthanikka26@users.noreply.github.com>
2022-07-06 01:58:14 +0530

    Create config.js

--- config.js
+++ config.js
@@ -42,13 +42,13 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['94789845893'] //ur owner number
-global.ownername = "🍭 ⃝𝐍𝐈𝐊𝐊𝐀 ⃝🍒"
+global.ownername = "NIKKA"
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "" //Cheemsbot MD V3
+global.botname = "Cheemsbot MD V3" //ur bot name
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
